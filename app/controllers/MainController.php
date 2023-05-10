c94c9670ba13af0dfc162e4df1e7ed22c3253519
Ars8 <pogosov-91@mail.ru>
2023-05-10 23:01:04 +0300

    class View part 2

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -9,6 +9,12 @@ class MainController extends AppController{
     public function IndexAction() {
         //echo __METHOD__;
         //$this->layout = 'test';
+        $this->setMeta('Главная страница', 'Описание...', 'Ключевики...');
+
+        $name = 'John';
+        $age = 30;
+        $names = ['Andrey', 'Jane',];
+        $this->set(compact('name', 'age', 'names'));
     }
 
 }
--- vendor/ishop/core/base/View.php
+++ vendor/ishop/core/base/View.php
@@ -28,6 +28,7 @@ class View {
     }
 
     public function render($data) {
+        if(is_array($data)) extract($data);
         $viewFile = APP . "/views/{$this->prefix}{$this->controller}/{$this->view}.php";
         if(is_file($viewFile)){
             ob_start();
@@ -44,5 +45,12 @@ class View {
                 throw new \Exception("Не найден шаблон {$this->layout}", 500);
             }
         }
+    }
+
+    public function getMeta() {
+        $output = '<title>' . $this->meta['title']. '</title>' . PHP_EOL;
+        $output .= '<meta name="description" content="'. $this->meta['desc']. '">' . PHP_EOL;
+        $output.= '<meta name="keywords" content="'. $this->meta['keywords']. '">' . PHP_EOL;
+        return $output;
     }
 }
