1026f7e2bf23b3953abd840a4eae307568bbcd9d
Ars8 <pogosov-91@mail.ru>
2023-05-12 13:34:32 +0300

    connection layout

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -7,23 +7,7 @@ use ishop\Cache;
 class MainController extends AppController{
 
     public function IndexAction() {
-        $posts = \R::findAll('test');
-        $post = \R::findOne('test', 'id = ?', [2]);
-        //debug($post);
         $this->setMeta('Главная страница', 'Описание...', 'Ключевики...');
-
-        $name = 'John';
-        $age = 30;
-        $names = ['Andrey', 'Jane', 'Mike'];
-        $cache = Cache::instance();
-        //$cache->set('test', $name);
-        //$cache->delete('test');
-        $data = $cache->get('test');
-        if(!$data) {
-            $cache->set('test', $names);
-        }
-        debug($data);
-        $this->set(compact('name', 'age', 'names', 'posts'));
     }
 
 }
